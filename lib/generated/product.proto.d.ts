c65706d0b1f1c31783b2b41e16b6befeb097ca88
suk-6 <me@suk.kr>
2024-08-25 04:16:26 +0900

    feat: Add product option and protocol files

---
+++ lib/generated/product.proto.d.ts
@@ -0,0 +1,23 @@
+import { Observable } from "rxjs";
+import { SuccessResponse, Uid } from "./messages/messages.proto";
+export declare const protobufPackage = "product";
+export interface CreateProduct {
+    title: string;
+    companyId: string;
+}
+export interface ProductResponse {
+    uid: string;
+    title: string;
+    companyId: string;
+}
+export declare const PRODUCT_PACKAGE_NAME = "product";
+export interface ProductServiceClient {
+    createProduct(request: CreateProduct): Observable<SuccessResponse>;
+    getProduct(request: Uid): Observable<ProductResponse>;
+}
+export interface ProductServiceController {
+    createProduct(request: CreateProduct): Promise<SuccessResponse> | Observable<SuccessResponse> | SuccessResponse;
+    getProduct(request: Uid): Promise<ProductResponse> | Observable<ProductResponse> | ProductResponse;
+}
+export declare function ProductServiceControllerMethods(): (constructor: Function) => void;
+export declare const PRODUCT_SERVICE_NAME = "ProductService";
