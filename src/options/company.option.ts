8e89a755e84d15ee28c38c1c8c36e14500bd7f13
suk-6 <me@suk.kr>
2024-08-25 03:34:16 +0900

    feat: Add company option and protocol files

---
+++ src/options/company.option.ts
@@ -0,0 +1,17 @@
+import { addReflectionToGrpcConfig } from "nestjs-grpc-reflection";
+import { join } from "path";
+
+import { GrpcOptions, Transport } from "@nestjs/microservices";
+
+import { getServiceHost } from "../constants/env";
+
+export const servicePort = 30002;
+
+export const grpcClientOptions: GrpcOptions = addReflectionToGrpcConfig({
+  transport: Transport.GRPC,
+  options: {
+    url: `${getServiceHost("company")}:${servicePort}`,
+    package: "company",
+    protoPath: join(__dirname, "..", "protocols", "company.proto"),
+  },
+});
